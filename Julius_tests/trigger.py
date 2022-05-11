1761b8fb86b8bd636016963f177e4df40ba11417
juliomendozah <julio.mendozah@pucp.edu.pe>
2022-05-10 21:34:59 -0500

    "To ECR"

--- Julius_tests/trigger.py
+++ Julius_tests/trigger.py
@@ -4,9 +4,10 @@ from datetime import datetime
 import requests
 
 #aws_api_url = "https://gffncchj4d.execute-api.us-east-1.amazonaws.com/test/trigger"
-aws_api_url = "https://ifl30z1ca5.execute-api.us-east-2.amazonaws.com/default/Stream_Video"
+#aws_api_url = "https://ifl30z1ca5.execute-api.us-east-2.amazonaws.com/default/Stream_Video"
 #aws_api_url = "https://1hltqctd35.execute-api.us-east-2.amazonaws.com/default/Predictor"
-
+#With container
+aws_api_url = "https://dttju25fc6.execute-api.us-east-2.amazonaws.com/default/jolepe"
 # The callback for when a PUBLISH message is received from the server.
 def trigger_lambda():
 
