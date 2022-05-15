2f3f03c94960a01206b3c25918ab67ba24eda302
juliomendozah <julio.mendozah@pucp.edu.pe>
2022-05-15 17:28:53 -0500

    "Update"

--- 3.Classification/Stream/app/init_mp_holistic.py
+++ 3.Classification/Stream/app/init_mp_holistic.py
@@ -8,7 +8,7 @@ holistic = mp_holistic.Holistic(static_image_mode=True,
                                     min_detection_confidence=0.5,
                                     min_tracking_confidence=0.5)
 
-file = 'data/holistic.jpg'
+file = './data/holistic.jpg'
 image = cv2.imread(file)
 # Convert the BGR image to RGB before processing.
 results = holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
